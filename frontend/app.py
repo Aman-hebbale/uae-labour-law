40f0ef4ea7adde66aae8eab1206cde8b0315cef1
Aman H <aman.hebbale@gmail.com>
2026-03-06 21:53:23 +0530

    changing frontend port

--- frontend/app.py
+++ frontend/app.py
@@ -9,5 +9,5 @@ st.title("GCP RAG Assistant")
 
 query = st.text_input("Ask me anything about your data:")
 if st.button("Send"):
-    res = requests.get(f"http://136.116.16.132:8501/ask", params={"query": query})
+    res = requests.get(f"http://136.116.16.132:8000/ask", params={"query": query})
     st.write(res.json().get("answer"))
