8073f1b787c55bf7dcd59456299a2ab893155a57
Aman H <aman.hebbale@gmail.com>
2026-03-06 23:04:59 +0530

    changing frontend port

--- frontend/app.py
+++ frontend/app.py
@@ -11,5 +11,5 @@ query = st.text_input("Ask me anything about your data:")
 # res = requests.get(f"http://136.116.16.132:8000/ask", params={"query": "what is wage"})
 # print(res.json())
 if st.button("Send"):
-    res = requests.get(f"http://10.128.0.3:8000/ask", params={"query": query})
+    res = requests.get(f"http://136.116.16.132:8000/ask", params={"query": query})
     st.write(res.json().get("answer"))
