6d8d8ac831e4e9bb7cf6344547d7090ef42046c0
Aman H <aman.hebbale@gmail.com>
2026-03-06 22:35:39 +0530

    changing frontend port

--- frontend/app.py
+++ frontend/app.py
@@ -3,11 +3,13 @@ import requests
 import os
 
 # BACKEND_URL = os.getenv("BACKEND_URL")
-BACKEND_URL = os.getenv("BACKEND_URL")
+# BACKEND_URL = os.getenv("BACKEND_URL")
 
 st.title("GCP RAG Assistant")
 
 query = st.text_input("Ask me anything about your data:")
+# res = requests.get(f"http://136.116.16.132:8000/ask", params={"query": "what is wage"})
+# print(res.json())
 if st.button("Send"):
-    res = requests.get(f"http://136.116.16.132:8000/ask", params={"query": query})
+    res = requests.get(f"http://localhost:8000/ask", params={"query": query})
     st.write(res.json().get("answer"))
